65cf6ab82059e51eb0c50b74c3393246daeaa7ef
Bassam Naji <bassam1881999@gmail.com>
2022-06-13 20:28:05 +0400

    finish iterators and half of vectors

--- srcs/iterator.hpp
+++ srcs/iterator.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   iterator.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bnaji <bnaji@student.42abudhabi.ae>        +#+  +:+       +#+        */
+/*   By: bnaji <bnaji@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 10:14:34 by bnaji             #+#    #+#             */
-/*   Updated: 2022/06/05 14:44:35 by bnaji            ###   ########.fr       */
+/*   Updated: 2022/06/13 15:01:48 by bnaji            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,39 @@
 
 namespace ft
 {
+  struct input_iterator_tag {};
+  struct output_iterator_tag {};
+  struct forward_iterator_tag : public input_iterator_tag {};
+  struct bidirectional_iterator_tag : public forward_iterator_tag {};
+  struct random_access_iterator_tag : public bidirectional_iterator_tag {};
+
+  template <class Iterator> struct iterator_traits
+{
+   typedef typename Iterator::value_type        value_type;
+   typedef typename Iterator::difference_type   difference_type;
+   typedef typename Iterator::pointer           pointer;
+   typedef typename Iterator::reference         reference;
+   typedef typename Iterator::iterator_category iterator_category;
+};
+  
+  template <class T> struct iterator_traits<T*>
+  {
+    typedef T                                   value_type;
+    typedef ptrdiff_t                           difference_type;
+    typedef T*                                  pointer;
+    typedef T&                                  reference;
+    typedef random_access_iterator_tag          iterator_category;
+  };
+  
+  template <class T> struct iterator_traits<const T*>
+  {
+    typedef T                                   value_type;
+    typedef ptrdiff_t                           difference_type;
+    typedef T*                                  pointer;
+    typedef T&                                  reference;
+    typedef random_access_iterator_tag          iterator_category;
+  };
+  
   template <class Category, class T, class Distance = std::ptrdiff_t,
 	  	class Pointer = T*, class Reference = T&>
 	class iterator
@@ -29,25 +62,25 @@ namespace ft
         typedef Distance                         difference_type;
         typedef Pointer                          pointer;
         typedef Reference                        reference;
-        typedef std::random_access_iterator_tag  iterator_category;
+        typedef Category                         iterator_category;
         
       iterator() : _p(NULL) {}
       iterator(iterator const & src) : _p(NULL) { *this = src; }
       ~iterator() {}
       
-      pointer                  base() const { return _p; }
+      pointer                   base() const { return _p; }
       
       iterator &		            operator = ( iterator const & rhs ) { if (this != &rhs) { _p  = rhs._p; } return this; }
-      iterator  		            operator + ( difference_type const & n ) const { reutrn (_p + n); }
-      iterator  		            operator - ( difference_type const & n ) const { reutrn (_p - n); }
+      iterator  		            operator + ( difference_type & n ) const { reutrn (_p + n); }
+      iterator  		            operator - ( difference_type & n ) const { reutrn (_p - n); }
 
-      iterator &                operator += (difference_type const & n) { _p = _p + n; return *this; }
+      iterator &                operator += (difference_type n) { _p = _p + n; return *this; }
       iterator &                operator ++ () { ++_p; return *this; }
-      iterator                  operator ++ (difference_type) { iterator tmp(*this); _p++; return (tmp); }
+      iterator                  operator ++ (int) { iterator tmp(*this); _p++; return (tmp); }
       
-      iterator &                operator -= (difference_type const & n) { _p = _p - n; return *this; }
+      iterator &                operator -= (difference_type n) { _p = _p - n; return *this; }
       iterator &                operator -- () { --_p; return *this; }
-      iterator                  operator -- (difference_type) { iterator tmp(*this); _p--; return (tmp); }
+      iterator                  operator -- (int) { iterator tmp(*this); _p--; return (tmp); }
       
       bool                    	operator == ( iterator const & rhs ) { return ((_p == rhs._p) ? true : false); }
       bool             	      	operator != ( iterator const & rhs ) { return ((_p != rhs._p) ? true : false); }
@@ -56,65 +89,107 @@ namespace ft
       bool             	      	operator > ( iterator const & rhs ) { return ((_p > rhs._p) ? true : false); }
       bool             	      	operator < ( iterator const & rhs ) { return ((_p < rhs._p) ? true : false); }
 
-      reference                 operator * () { return *(_p); }
-      reference const           operator * () const { return *(_p); }
-      pointer                   operator -> () { return _p; }
+      reference                 operator * () const { return *(_p); }
+      pointer                   operator -> () const { return _p; }
 
       reference                 operator [] (difference_type const & n) { return _p[n]; } 
-      reference const           operator [] (difference_type const & n) const { return _p[n]; }
+
+    private:
+      pointer     _p;
+	};
+  
+  
+  template <class Category, class T, class Distance = std::ptrdiff_t,
+	  	class Pointer = T*, class Reference = T&>
+	class reverse_iterator
+	{
+    public:
+        typedef T                                value_type;
+        typedef Distance                         difference_type;
+        typedef Pointer                          pointer;
+        typedef Reference                        reference;
+        typedef Category                         iterator_category;
+        
+      reverse_iterator() : _p(NULL) {}
+      reverse_iterator(reverse_iterator const & src) : _p(NULL) { *this = src; }
+      ~reverse_iterator() {}
+      
+      pointer                   base() const { return _p; }
+      
+      reverse_iterator &	      operator = ( reverse_iterator const & rhs ) { if (this != &rhs) { _p  = rhs._p; } return this; }
+      reverse_iterator  	      operator + ( difference_type & n ) const { reutrn (_p - n); }
+      reverse_iterator  	      operator - ( difference_type & n ) const { reutrn (_p + n); }
+
+      reverse_iterator &        operator += (difference_type n) { _p = _p - n; return *this; }
+      reverse_iterator &        operator ++ () { --_p; return *this; }
+      reverse_iterator          operator ++ (int) { reverse_iterator tmp(*this); _p--; return (tmp); }
+      
+      reverse_iterator &        operator -= (difference_type n) { _p = _p + n; return *this; }
+      reverse_iterator &        operator -- () { ++_p; return *this; }
+      reverse_iterator          operator -- (int) { reverse_iterator tmp(*this); _p++; return (tmp); }
+      
+      bool                    	operator == ( reverse_iterator const & rhs ) { return ((_p == rhs._p) ? true : false); }
+      bool             	      	operator != ( reverse_iterator const & rhs ) { return ((_p != rhs._p) ? true : false); }
+      bool             	      	operator >= ( reverse_iterator const & rhs ) { return ((_p >= rhs._p) ? true : false); }
+      bool             	      	operator <= ( reverse_iterator const & rhs ) { return ((_p <= rhs._p) ? true : false); }
+      bool             	      	operator > ( reverse_iterator const & rhs ) { return ((_p > rhs._p) ? true : false); }
+      bool             	      	operator < ( reverse_iterator const & rhs ) { return ((_p < rhs._p) ? true : false); }
+
+      reference                 operator * () const { return *(_p); }
+      pointer                   operator -> () const { return _p; }
+
+      reference                 operator [] (difference_type n) const { return _p[n]; } 
 
     private:
       pointer     _p;
 	};
 
+  // template <class Iterator>
+  // bool operator == (const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs) { return ((_p == rhs._p) ? true : false); }
+
+  // template <class Iterator>
+  // bool operator != (const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs) { return ((_p != rhs._p) ? true : false); }
+
+  // template <class Iterator>
+  // bool operator >= (const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs) { return ((_p >= rhs._p) ? true : false); }
+
+  // template <class Iterator>
+  // bool operator <= (const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs) { return ((_p <= rhs._p) ? true : false); }
+
+  // template <class Iterator>
+  // bool operator > (const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs) { return ((_p > rhs._p) ? true : false); }
+
+  // template <class Iterator>
+  // bool operator < (const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs) { return ((_p < rhs._p) ? true : false); }
   
-  // template <class Category, class T, class Distance = std::ptrdiff_t,
-	//   	class Pointer = T*, class Reference = T&>
-	// class reverse_iterator
-	// {
-  //   public:
-  //       typedef T                                value_type;
-  //       typedef Distance                         difference_type;
-  //       typedef Pointer                          pointer;
-  //       typedef Reference                        reference;
-  //       typedef std::random_access_iterator_tag  iterator_category;
-        
-  //     reverse_iterator() : _p(NULL) {}
-  //     reverse_iterator(reverse_iterator(); const & src) : _p(NULL) { *this = src; }
-  //     ~reverse_iterator() {}
-      
-  //     pointer                  base() const { return _p; }
-      
-  //     iterator &		            operator = ( iterator const & rhs ) { if (this != &rhs) { _p  = rhs._p; } return this; }
-  //     iterator  		            operator + ( difference_type const & n ) const { reutrn (_p + n); }
-  //     iterator  		            operator - ( difference_type const & n ) const { reutrn (_p - n); }
+  // template <class Iterator>
+  // reverse_iterator<Iterator> operator + (typename reverse_iterator<Iterator>::difference_type n, const reverse_iterator<Iterator>& rev_it) { return (n + rev_it); }
+  
+  // template <class Iterator>
+  // typename reverse_iterator<Iterator>::difference_type operator - (const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs) { return (lhs - rhs); }
 
-  //     iterator &                operator += (difference_type const & n) { _p = _p + n; return *this; }
-  //     iterator &                operator ++ () { ++_p; return *this; }
-  //     iterator                  operator ++ (int) { if (_p) { iterator tmp(*this); _p++; return (tmp); } }
-      
-  //     iterator &                operator -= (difference_type const & n) { _p = _p - n; return *this; }
-  //     iterator &                operator -- () { --_p; return *this; }
-  //     iterator                  operator -- (int) { if (_p) { iterator tmp(*this); _p--; return (tmp); } }
-      
-  //     bool                    	operator == ( iterator const & rhs ) { return ((_p == rhs._p) ? true : false); }
-  //     bool             	      	operator != ( iterator const & rhs ) { return ((_p != rhs._p) ? true : false); }
-  //     bool             	      	operator >= ( iterator const & rhs ) { return ((_p >= rhs._p) ? true : false); }
-  //     bool             	      	operator <= ( iterator const & rhs ) { return ((_p <= rhs._p) ? true : false); }
-  //     bool             	      	operator > ( iterator const & rhs ) { return ((_p > rhs._p) ? true : false); }
-  //     bool             	      	operator < ( iterator const & rhs ) { return ((_p < rhs._p) ? true : false); }
+  template<class Iterator>
+  typename iterator_traits<Iterator>::difference_type
+  distance (Iterator first, Iterator last)
+  {
+    return (distance(first, last, iterator_traits<Iterator>::iterator_category()));
+  }
 
-  //     reference                 operator * () { return *(_p); }
-  //     reference const           operator * () const { return *(_p); }
-  //     pointer                   operator -> () { return _p; }
+  template<class RandomAccessIterator>
+  typename iterator_traits<RandomAccessIterator>::difference_type
+  distance (RandomAccessIterator first, RandomAccessIterator last, std::random_access_iterator_tag)
+  {
+    return last - first;
+  }
 
-  //     reference                 operator [] (difference_type const & n) { return _p[n]; } 
-  //     reference const           operator [] (difference_type const & n) const { return _p[n]; }
-
-  //   private:
-  //     pointer     _p;
-	// };
-
+  template<class InputIterator>
+  typename iterator_traits<InputIterator>::difference_type
+  distance (InputIterator first, InputIterator last, std::input_iterator_tag)
+  {
+    typename iterator_traits<InputIterator>::difference_type diff;
+    for(diff = 0; first != last; first++, diff++);
+    return diff;
+  }
   
 }
 
--- srcs/vector.hpp
+++ srcs/vector.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   vector.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bnaji <bnaji@student.42abudhabi.ae>        +#+  +:+       +#+        */
+/*   By: bnaji <bnaji@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 13:15:13 by bnaji             #+#    #+#             */
-/*   Updated: 2022/06/05 15:55:08 by bnaji            ###   ########.fr       */
+/*   Updated: 2022/06/13 20:27:13 by bnaji            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,36 +28,138 @@ namespace ft
 
 		public:
 
-			typedef T																																					value_type;
-			typedef Alloc																																			allocator_type;
-			typedef typename allocator_type::reference																				reference;
-			typedef typename allocator_type::const_reference																	const_reference;
-			typedef typename allocator_type::pointer																					pointer;
-			typedef typename allocator_type::const_pointer																		const_pointer;
-			typedef typename ft::iterator<std::random_access_iterator_tag, value_type>				iterator;
-			typedef typename ft::iterator<std::random_access_iterator_tag, const value_type>	const_iterator;
-			typedef ptrdiff_t																																	difference_type;
-			typedef size_t																																		size_type;
+			typedef T																																				          	value_type;
+			typedef Alloc																																		          	allocator_type;
+			typedef typename allocator_type::reference																			          	reference;
+			typedef typename allocator_type::const_reference																          	const_reference;
+			typedef typename allocator_type::pointer																				          	pointer;
+			typedef typename allocator_type::const_pointer																	          	const_pointer;
+			typedef typename ft::iterator<std::random_access_iterator_tag, value_type>			            iterator;
+			typedef typename ft::iterator<std::random_access_iterator_tag, const value_type>            const_iterator;
+			typedef typename ft::reverse_iterator<std::random_access_iterator_tag, value_type>          reverse_iterator;
+			typedef typename ft::reverse_iterator<std::random_access_iterator_tag, const value_type>    const_reverse_iterator;
+			typedef ptrdiff_t																																           	difference_type;
+			typedef size_t																																	           	size_type;
 			
-			explicit vector (const allocator_type & alloc = allocator_type()) : _alloc(alloc), _arr(NULL), _size(0), _capacity(0) {}
-			explicit vector (size_type n, const value_type & val = value_type(),
-						const allocator_type & alloc = allocator_type()) : _alloc(alloc), _arr(NULL), _size(n), _capacity(n) { _arr = _alloc.allocate(n); for (size_t i = 0; i < n; i++) { _arr[i] = val; } }
+      /* ************************************** Constructors ************************************** */
+			explicit vector (const allocator_type & alloc = allocator_type()) : _alloc(alloc), _arr(NULL), _size(0), _capacity(0) 
+      {}
+			explicit vector (size_type n, const value_type & val = value_type(), const allocator_type & alloc = allocator_type()) : _alloc(alloc), _arr(NULL), _size(n), _capacity(n * 2)
+      {
+        _arr = _alloc.allocate(n);
+        for (size_t i = 0; i < n; i++)
+          _alloc.construct(_arr + i, val);
+      }
 			template <class InputIterator>
-					vector (InputIterator first, InputIterator last,
-									const allocator_type& alloc = allocator_type()) {}
-			vector (const vector& x) {}
-			~vector() {}
+			vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()) : _alloc(alloc), _arr(NULL)
+      {
+        size_type n = ft::distance(first, last);
+        _size = n;
+        _capacity = n * 2;
+        _arr = _alloc.allocate(n);
+        for (size_t i = 0; i < n; i++)
+          _alloc.construct(_arr + i, *first++);
+      }
+			vector (const vector & x) : _alloc(allocator_type()), _arr(NULL), _size(0), _capacity(0)
+      {
+        *this = x;
+      }
+			~vector()
+      {
+        for (size_type i = 0; i < _size; i++) {
+          _alloc.destroy(_arr + i);
+        }
+        _alloc.deallocate(_arr, _capacity);
+      }
+      
 
-			vector &		operator=( vector const & rhs ) {}
 
-			iterator						begin() { return _arr; }
-			const_iterator 			begin() const { return _arr; }
+      /* ************************************** Operators ************************************** */
+			vector &		operator=( vector const & rhs )
+      {
+        if (this != rhs)
+        {
+          if (rhs._size > _capacity)
+          {
+            size_type i;
+            for (i = 0; i != _size ; i++) {
+              _alloc.destroy(_arr + i);
+            }
+            _alloc.deallocate(_arr, _size);
+            _arr = _alloc.allocate(rhs._capacity);
+            for (i = 0 ; i != rhs._size; i++) {
+              _arr[i] = rhs._arr[i];
+            }
+          }
+          else
+          {
+            size_type i = 0;
+            for ( ; i < rhs._size; i++)
+              _arr[i] = rhs._arr[i];
+          }
+          _size = rhs._size;
+          _capacity = rhs._capacity;
+        }
+        return *this;
+      }
 
-			
-			void func() {
-				std::cout << "Func" << std::endl;
-			}
+      /* ************************************** Iterators ************************************** */
+			iterator					        	begin() { return _arr; }
+			const_iterator 		        	begin() const { return _arr; }
+			iterator					        	end() { return (_arr + size()); }
+			const_iterator 		        	end() const { return (_arr + size()); }
+			reverse_iterator						rbegin() { return (_arr + size()); }
+			const_reverse_iterator 			rbegin() const { return (_arr + size()); }
+			reverse_iterator						rend() { return _arr; }
+			const_reverse_iterator 			rend() const { return _arr; }
 
+      /* ************************************** Capacity ************************************** */
+      size_type size() const { return _size; }
+      size_type max_size() const { return (sizeof(void *) * 8); }
+      size_type capacity() const { return _capacity; }
+      bool      empty() const { return (_size != 0 ? false : true); }
+      void      resize(size_type n, value_type val = value_type())
+      {
+        if (n > _size && n > _capacity)
+        {
+          T * tmp = _alloc.allocate(_size);
+          size_type i = 0;
+          for ( ; i != _size ; i++) {
+            tmp[i] = _arr[i];
+            _alloc.destroy(_arr + i);
+          }
+          _alloc.deallocate(_arr, _size);
+          _size = n;
+          _arr = _alloc.allocate(_size);
+          size_type j = 0;
+          for ( ; j != i; j++) {
+            _arr[j] = tmp[j];
+            _alloc.destroy(tmp + j);
+          }
+          _alloc.deallocate(tmp, _size);
+          for ( ; j != _size; j++)
+            _arr[j] = val;
+        }
+        else if (n > _size && n <= _capacity)
+        {
+          size_type i = 0;
+          for ( ; i + _size < n; i++)
+            _arr[i] = val;
+          _size += i;
+        }
+        else if (n < _size)
+        {
+          size_type i = _size - 1;
+          for ( ; i > n; i--)
+            _arr[i] = value_type();
+          _size = i;
+        }
+      }
+
+      /* ************************************** Element Access ************************************** */
+      /* ************************************** Modifiers ************************************** */
+      /* ************************************** Allocator ************************************** */
+      
 		private:
 			allocator_type	_alloc;
 			T *							_arr;
@@ -66,5 +168,6 @@ namespace ft
 
 	};
 }
+  /* ************************************** Non-member functions Overloads ************************************** */
 
 #endif /* ********************************************************** VECTOR_H */
